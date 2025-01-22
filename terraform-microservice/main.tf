393ec35cf5409a615394908c0dbf5a13eff1f8b0
mercadoalex <mercadoalex@gmail.com>
2025-01-21 19:54:12 -0600

    using existing keys

--- terraform-microservice/main.tf
+++ terraform-microservice/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "microservice" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.main.key_name
+  key_name                    = "las_llaves" 
   vpc_security_group_ids      = [aws_security_group.main.id]
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
